befd92bd909755578e50a2058776b86cc509cfc7
Rishabh Saxena <rishabhsaxena1998@gmail.com>
2023-11-15 03:56:04 +0530

    Add ngRx dev tools for debugging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
-
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
 import { AppReducer } from './store/app.reducer';
+import { environment } from 'src/environment/environment';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -21,6 +22,10 @@ import { AppReducer } from './store/app.reducer';
     SharedModule,
     StoreModule.forRoot(AppReducer),
     EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      logOnly: environment.production,
+      trace: true,
+    }),
   ],
   /* If there are more objects in providers, we can move them to a 
   separate CoreModule */
