a43a5cebc7d82e7e5564c48ec741be0160eca70e
Rishabh Saxena <rishabhsaxena1998@gmail.com>
2023-11-16 21:03:42 +0530

    Fix code in Resolver according to the NgRx implementation

--- src/app/recipes/recipe-resolver.service.ts
+++ src/app/recipes/recipe-resolver.service.ts
@@ -3,27 +3,43 @@ import {
   Resolve,
   RouterStateSnapshot,
 } from '@angular/router';
-import { Recipe } from './recipe.model';
-import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { DataStorageService } from '../shared/data-storage.service';
-import { RecipeService } from './recipe.service';
+import { Observable, of } from 'rxjs';
+import { map, switchMap, take } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import { Actions, ofType } from '@ngrx/effects';
+
+import { Recipe } from './recipe.model';
+import { AppState } from '../store/state';
+import { RecipeState } from './store/recipe.store';
+import * as RecipeActions from './store/recipe.actions';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecipeResolverService implements Resolve<Recipe[]> {
-  constructor(
-    private dataStorageService: DataStorageService,
-    private recipeService: RecipeService
-  ) {}
+  constructor(private store: Store<AppState>, private actions$: Actions) {}
 
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Recipe[] | Observable<Recipe[]> | Promise<Recipe[]> {
-    const recipes: Recipe[] = this.recipeService.getRecipes();
-    if (recipes.length === 0) return this.dataStorageService.fetchRecipes();
-    return recipes;
+    return this.store.select('recipe').pipe(
+      take(1),
+      map((recipeState: RecipeState) => recipeState.recipes),
+      switchMap((recipes) => {
+        // return recipes if already present in store
+        if (recipes.length > 0) return of(recipes);
+
+        // else fetch recipees
+        this.store.dispatch(RecipeActions.FetchRecipes());
+        /* Resolver will wait for the action SET_RECIPES to be dispatched 
+        which is dispatched when recipes are fetched */
+        return this.actions$.pipe(
+          ofType(RecipeActions.RecipeActionTypes.SET_RECIPES),
+          take(1)
+        );
+      })
+    );
   }
 }
